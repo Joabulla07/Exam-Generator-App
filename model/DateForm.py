a93fdd5340b72fbd568fb2329f9b69deecfe0968
Joanna <40646853+Joabulla07@users.noreply.github.com>
2023-12-09 11:47:48 -0300

    validation on generate buttom

--- model/DateForm.py
+++ model/DateForm.py
@@ -1,8 +1,5 @@
-import sys
-from datetime import datetime
-from PySide6 import QtWidgets
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QWidget, QLabel, QFormLayout, QLineEdit, QPushButton, QMessageBox
+from PySide6.QtWidgets import QWidget, QLabel, QFormLayout, QLineEdit, QPushButton, QMessageBox
 
 from validations.utils import validate_start_date_and_today, validate_date_end, validate_date
 
@@ -17,23 +14,21 @@ class DateForm(QWidget):
         layout = QFormLayout()
         self.setLayout(layout)
 
-        # Crear los campos de fecha de inicio y fecha de fin
         self.start_date_edit = QLineEdit(self)
         self.end_date_edit = QLineEdit(self)
 
         layout.addRow(QLabel("Fecha de inicio (dd/mm/yy):"), self.start_date_edit)
         self.generate_start_buttom = QPushButton("Validar")
-        self.generate_start_buttom.setFixedSize(60, 20)  # Ajustar tamaño
+        self.generate_start_buttom.setFixedSize(60, 20)
         layout.addWidget(self.generate_start_buttom)
         self.generate_start_buttom.clicked.connect(self.validate_date_one)
 
         layout.addRow(QLabel("Fecha de fin (dd/mm/yy):"), self.end_date_edit)
         self.generate_end_buttom = QPushButton("Validar")
-        self.generate_end_buttom.setFixedSize(60, 20)  # Ajustar tamaño
+        self.generate_end_buttom.setFixedSize(60, 20)
         layout.addWidget(self.generate_end_buttom)
         self.generate_end_buttom.clicked.connect(self.validate_date_two)
 
-        # Crear el botón "Generar"
         self.generate_button = QPushButton("Generar", self)
         layout.addWidget(self.generate_button)
         self.generate_button.clicked.connect(self.generate_period)
@@ -46,6 +41,7 @@ class DateForm(QWidget):
             if validate_start_date_and_today(date):
                 icon = QIcon("images/check.png")
                 buttom.setIcon(icon)
+                return True
             else:
                 raise Exception
         except:
@@ -60,6 +56,7 @@ class DateForm(QWidget):
             if validate_date_end(end_date, start_date):
                 icon = QIcon("images/check.png")
                 buttom.setIcon(icon)
+                return True
             else:
                 raise Exception
         except:
@@ -68,5 +65,7 @@ class DateForm(QWidget):
     def generate_period(self):
         start_date = self.start_date_edit.text()
         end_date = self.end_date_edit.text()
-        period = {"start_date": start_date, "end_date": end_date}
-        print(period)
+        if self.validate_date_one() and self.validate_date_two():
+            period = {"start_date": start_date, "end_date": end_date}
+            print(period)
+
