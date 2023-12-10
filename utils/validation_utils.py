617059cb2b00237b4233add4d0206e7fb383f7cd
Joanna <40646853+Joabulla07@users.noreply.github.com>
2023-12-09 22:13:30 -0300

    validate holiday and weekend days

--- utils/validation_utils.py
+++ utils/validation_utils.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+import holidays
+
 
 def validate_date(date):
     datetime.strptime(date, '%d/%m/%y')
@@ -29,4 +31,19 @@ def validate_date_end(end_date, start_date):
     start_date = datetime.strptime(start_date, '%d/%m/%y')
     if end_date > start_date:
         return True
+    return False
+
+
+def validate_date_is_not_holiday_or_weekend(date):
+    """
+    Return True if the date is not in the holiday or weekend
+    :param date:
+    :return:
+    """
+    ar_holidays = holidays.country_holidays(country="AR")
+    date = datetime.strptime(date, '%d/%m/%y')
+    if date.weekday() != 5 and date.weekday() != 6:
+        if ar_holidays.get(str(date.date())) is None:
+            return True
+        return False
     return False
