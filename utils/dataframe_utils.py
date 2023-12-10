6d2d8fd688187b3b685cd215aa5ef81845bb6c53
Joanna <40646853+Joabulla07@users.noreply.github.com>
2023-12-09 21:26:10 -0300

    add read dataframe and sort

---
+++ utils/dataframe_utils.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+def convert_excel_to_dataframe():
+    df = pd.read_excel("../docs/materias.xlsx")
+    return df
+
+
+def divide_dataframe_in_grade(df):
+    columns = df.columns.values.tolist()
+    df.columns = [col.lower() for col in columns]
+    groups = df.groupby(df.grado)
+    grade_1 = groups.get_group(1)
+    grade_2 = groups.get_group(2)
+    grade_3 = groups.get_group(3)
+    grade_4 = groups.get_group(4)
+    grade_5 = groups.get_group(5)
+    return grade_1, grade_2, grade_3, grade_4, grade_5
+
+
+def sort_dataframe(df):
+    return df.sort_values(by="materia")
